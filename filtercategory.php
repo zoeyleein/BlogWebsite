03cf8dd04e4c3d0b0f6955c181c44f38eff0b98c
JingYi Li <li000848@algonquinlive.com>
2023-11-27 21:25:05 -0500

    consistent Indentation for filter

--- filtercategory.php
+++ filtercategory.php
@@ -1,3 +1,9 @@
+<!-- 
+    Student: JingYi Li, Wei Deng
+    File Name: filterauthor.php
+    Date of creating: Nov 27 2023
+    Description: This is for author filter.
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,35 +35,35 @@ else{
 
 
 <table class="list">
-  	  <tr>
+    <tr>
         <th >ID</th>
         <th >Author</th>
         <th >Title</th>
         <th >Category</th>
-  	    <th >Content</th>
+        <th >Content</th>
         <th >Comment</th>
-  	    <th colspan="3">&nbsp;</th>
-  	  </tr>
+        <th colspan="3">&nbsp;</th>
+    </tr>
 
-      <?php while($results = mysqli_fetch_assoc($result_set)) { ?> 
+    <?php while($results = mysqli_fetch_assoc($result_set)) { ?> 
         <!-- get the data from $result_set and store it in $results -->
-      <tr>
+    <tr>
         <td><?php echo $results['id']; ?></td>
         <td><?php echo $results['author']; ?></td>
         <td><?php echo $results['title']; ?></td>
         <td><?php echo $results['category'] ; ?></td>
-    	  <td><?php echo $results['content']; ?></td>
+        <td><?php echo $results['content']; ?></td>
         <td><?php echo $results['comment']; ?></td>
         <td colspan="3">
-          <a class="action action-display" style="width: 55px;" href="<?php echo "show.php?id=" . $results['id']; ?>">Display</a>
-          <a class="action action-edit" style="width: 55px;" href="<?php echo "edit.php?id=" . $results['id']; ?>">Edit</a>
-          <a class="action action-remove" style="width: 55px;" href="<?php echo "delete.php?id=" . $results['id']; ?>">Remove</a>
-    	</tr>
-      <?php } ?>
-  </table>
+        <a class="action action-display" style="width: 55px;" href="<?php echo "show.php?id=" . $results['id']; ?>">Display</a>
+        <a class="action action-edit" style="width: 55px;" href="<?php echo "edit.php?id=" . $results['id']; ?>">Edit</a>
+        <a class="action action-remove" style="width: 55px;" href="<?php echo "delete.php?id=" . $results['id']; ?>">Remove</a>
+        </tr>
+    <?php } ?>
+</table>
 
-  <?php include("footerEm.php"); ?>
+    <?php include("footerEm.php"); ?>
 
-  </div>
+</div>
 </body>
 </html>
